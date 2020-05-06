11cfe0579b2ea888c364da97e0605e757eb8ab7a
Ben Lovy <ben@deciduously.com>
2020-05-05 20:35:34 -0400

    Add dynamic library directly

--- lib/rollenspielsache_svc.rb
+++ lib/rollenspielsache_svc.rb
@@ -1,6 +1,24 @@
-require "rollenspielsache_svc/version"
+# frozen_string_literal: true
 
+require 'rollenspielsache_svc/version'
+
+require 'librollenspielsache'
+
+# Service coordination for Rollenspielesache applications
 module RollenspielsacheSvc
+  include Librollenspielsache
   class Error < StandardError; end
   # Your code goes here...
+  class StringRoller
+    attr_reader :input
+    def initialize(input)
+      @input = input
+    end
+
+    def result
+      puts "Rolling #{input}..."
+      result = Dice::Roll.from_str(input).execute.total
+      puts result
+    end
+  end
 end
